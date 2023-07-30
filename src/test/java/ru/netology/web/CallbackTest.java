ec5331974943d6146697e1d308c2c0103a32c6b2
nhegmus@gmail.com <nhegm>
2023-07-30 17:38:59 +0800

    Working tests added

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,23 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+class CallbackTest {
+
+    @Test
+    void shouldPassWhenAllDataIsCorrectTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
---
+++ src/test/java/ru/netology/web/ErrorTest.java
@@ -0,0 +1,75 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class ErrorTest {
+
+    @Test
+    void shouldNotPassWhenFieldNameIsEmptyTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("span[data-test-id=phone] > span > span[class=input__sub]").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotPassWhenFieldPhoneIsEmptyTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("span[data-test-id=name] > span > span[class=input__sub]").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotPassWhenFieldNameIsWrongTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Barabashka");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("span[data-test-id=name] > span > span[class=input__sub]").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNotPassWhenFieldNameIsWrongWithoutPhoneNumberTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Barabashka");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("span[data-test-id=name] > span > span[class=input__sub]").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNotPassWhenFieldPhoneIsWrongTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79270");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("span[data-test-id=phone] > span > span[class=input__sub]").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldNotPassWhenBothFieldsAreWrongTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Chivo");
+        form.$("[data-test-id=phone] input").setValue("+79270");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("span[data-test-id=name] > span > span[class=input__sub]").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+}
